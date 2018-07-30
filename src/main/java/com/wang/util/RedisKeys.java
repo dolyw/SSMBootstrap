3f7a5013daacae1981dafa3a151a12df66f94a41
随心 <1107224733@qq.com>
2018-07-30 11:19:55 +0800

    按照Alibaba Java Coding Guidelines插件扫描结果，修改代码规范

--- src/main/java/com/wang/util/RedisKeys.java
+++ src/main/java/com/wang/util/RedisKeys.java
@@ -2,13 +2,13 @@ package com.wang.util;
 
 /**
  * @Desc 获取Redis的Key
- * @Author wang926454
+ * @Author Wang926454
  * @Date 2018/7/30 10:38
  */
 public class RedisKeys {
     /**
      * @Desc 获取用户ID
-     * @Author wliduo
+     * @Author Wang926454
      * @Date 2018/7/30 10:39
      */
     public static String getUserKey(String key){
